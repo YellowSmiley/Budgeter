730af064c2a548eb375af2b068a9a92a018c696c
Mike Smith <mike.smith@accesso.com>
2023-03-30 08:27:46 +0100

    Add project files.

---
+++ server/Program.cs
@@ -0,0 +1,27 @@
+﻿using Grpc.Core;
+
+namespace server
+{
+    class Program
+    {
+        const int Port = 50052;
+
+        static void Main(string[] args)
+        {
+            Server server = null;
+
+            try
+            {
+                server = new Server()
+                {
+                    Services = { TransactionService.}
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
