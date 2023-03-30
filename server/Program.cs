30b43a565123690c7f7de678ff7c5bd6911ee717
Mike Smith <mike.smith@accesso.com>
2023-03-30 09:31:47 +0100

    Initial setup

--- server/Program.cs
+++ server/Program.cs
@@ -1,10 +1,16 @@
 ﻿using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace server
 {
-    class Program
+    internal class Program
     {
-        const int Port = 50052;
+        const int Port = 50051;
 
         static void Main(string[] args)
         {
@@ -14,13 +20,22 @@ namespace server
             {
                 server = new Server()
                 {
-                    Services = { TransactionService.}
-                }
-            }
-            catch (Exception)
-            {
+                    Ports = { new ServerPort("localhost", Port, SslServerCredentials.Insecure) }
+                };
 
+                server.Start();
+                Console.WriteLine("The server is listening on the port: " + Port);
+                Console.ReadKey();
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("The server failed to start: " + e.Message);
                 throw;
+            }
+            finally
+            {
+                if (server != null) 
+                    server.ShutdownAsync().Wait();
             }
         }
     }
